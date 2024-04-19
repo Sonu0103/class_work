6a76aa300886a1eb4fa19509074af619c9d0c308
Sonu0103 <sonuhp2002@gmail.com>
2024-04-19 11:33:39 +0545

    set up flutter project

---
+++ android/app/src/main/kotlin/com/sonu/class_app/class_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.sonu.class_app.class_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:class_app/app/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
